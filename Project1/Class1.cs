831bc69ae033f11f6d0091f83b82e06a9fab3169
302091 <302091@edu.mkcollege.ac.uk>
2020-01-28 13:54:06 +0000

    test

---
+++ Project1/Class1.cs
@@ -0,0 +1,24 @@
+﻿using System.Web.Mvc;
+
+namespace Fonlow.WebApiClientGen
+{
+    [Route("api/[controller]")]
+    public class ConectionControler : ControllerBase
+    {
+        /// <summary>
+        /// Trigger the API to generate WebApiClientAuto.cs for an established client API project.
+        /// </summary>
+        /// <param name="settings"></param>
+        /// <returns>OK if OK</returns>
+        [HttpPost]
+        public int Post()
+        {
+            return 1;
+        }
+
+        protected override void ExecuteCore()
+        {
+            throw new System.NotImplementedException();
+        }
+    }
+}
